0103d4dbf28fb679ec43dbcd971d6e5a55add544
abdelazizhanafi <abdelazizhaanafy@gmail.com>
2024-11-13 05:36:16 +0200

    create custom text field

--- lib/widgets/quick_invoice.dart
+++ lib/widgets/quick_invoice.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/custom_background_container.dart';
+import 'package:responsive_dash_board/widgets/custom_text_field.dart';
 import 'package:responsive_dash_board/widgets/latest_transaction.dart';
 import 'package:responsive_dash_board/widgets/quick_invoice_header.dart';
 
@@ -17,8 +18,13 @@ class QuickInvoice extends StatelessWidget {
             height: 24,
           ),
           LatestTransaction(),
+          Divider(
+            height: 48,
+          ),
+          CustomTextField(),
         ],
       ),
     );
   }
 }
+
